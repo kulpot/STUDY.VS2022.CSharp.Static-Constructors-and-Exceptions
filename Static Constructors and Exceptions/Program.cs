3f8fdccfd522071234e401bd28cae15aceaba794
Kulpot <kulpotwahu@gmail.com>
2023-09-11 01:52:47 +0800

    Add project files.

---
+++ Static Constructors and Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=p9u6lk92JRU&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=48
+//
+
+class Cow
+{
+    static int blah = 20;
+    //static int blah;
+    static Cow()
+    {
+        //blah = 20;
+        Console.WriteLine("static Cow()");
+        throw new Exception();
+    }
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        try
+        {
+            new Cow();
+        }
+        catch { }
+        new Cow();
+
+    }
+}
